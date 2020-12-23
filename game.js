b7ed57adfe79fb478348cef38605e50605c1bf5c
MVP Aditya <76063440+stupidgaming@users.noreply.github.com>
2020-12-23 13:39:24 +0530

    Update game.js

--- game.js
+++ game.js
@@ -1,3 +1,20 @@
+var time_stamp = 0; // Or Date.now()
+
+window.addEventListener("touchstart", function(event_) {
+
+    if (event_.timeStamp - time_stamp < 300) { // A tap that occurs less than 300 ms from the last tap will trigger a double tap. This delay may be different between browsers.
+
+        event_.preventDefault();
+
+        return false;
+
+    }
+
+    time_stamp = event_.timeStamp;
+
+});
+
+
 const game = ()=> {
 
     let pScore = 0; 
